74f0833f043bb30cab49d71ef5247aa54e604c9c
wallwpcab <walihullah@gmail.com>
2021-10-22 16:46:21 +0600

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+// @ts-ignore
 import { AppComponent } from './app.component';
+// @ts-ignore
+// @ts-ignore
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
